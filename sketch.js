2bd742227956ebd87745837de5206fdf3bc2c7c4
SAVARCHINAL <srarchinal@dons.usfca.edu>
2022-02-16 16:00:22 -0800

    revised
    first finish

--- sketch.js
+++ sketch.js
@@ -16,9 +16,22 @@ function preload() {
 }
 
 // Setup code goes here
-function setup() {
-  createCanvas(800, 800);
-  imageMode(CENTER);
+
+  
+  
+
+  function setup() {
+    var cnv = createCanvas(800, 700);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    cnv.position(x, y);
+    background(255, 0, 200);
+    imageMode(CENTER);
+ 
+  
+  
+
+
 
   // setup the state machine with callbacks
   simpleStateMachine.setup(setImage, setTransitionNames);
@@ -31,8 +44,11 @@ function draw() {
   drawBackground();
   background(219, 94, 81);
 
+
   drawImage();
   drawUI();
+  
+    
 }
 
 // this is a callback, which we use to set our display image
@@ -76,6 +92,7 @@ function keyPressed() {
 
 function drawBackground() {
   background(0);
+  textAlign(CENTER);
 }
 
 function drawImage() {
@@ -86,16 +103,18 @@ function drawImage() {
 
 function drawUI() {
   push();
-  textAlign(LEFT);
-  textSize(18);
+  textAlign(CENTER);
+  textSize(24);
+  textFont("Monospace");
+  
 
   for( let i = 0; i < transitions.length; i++ ) {
     fill(255);
 
     if( selectedTransitionNum === i ) {
-      fill(240,50,0);
+      fill(104, 145, 77);
     }
-    text( transitions[i], 100, (height - 100) + (i*30)  );
+    text( transitions[i], 370, (height - 50) + (i*30)  );
   }
 
   pop();
